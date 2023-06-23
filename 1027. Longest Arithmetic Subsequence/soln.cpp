8d310c801f9bdc92344109c74a05131b52edc077
suyogsabale <suyogsabale717@gmail.com>
2023-06-23 22:22:47 +0530

    upadated soln

--- 1027. Longest Arithmetic Subsequence/soln.cpp
+++ 1027. Longest Arithmetic Subsequence/soln.cpp
@@ -1,25 +1,19 @@
-
 class Solution {
-public:
-    int longestArithSeqLength(vector<int>& A) {
-        unordered_map<int, unordered_map<int, int>> dp;
-        int ans = 0;
-        
-        for(int i = 0; i < A.size(); i++){
-            for(int j = i+1; j < A.size(); j++){
-                int d = A[j]-A[i];
-                //sequence length of d and ends at j
-                /*
-                when dp[d][i] not exist, 
-                dp[d][j] is first of such sequence, 
-                so its length is 2.
-                otherwise, we can concat j to the previous sequence ends at i
-                */
-                dp[d][j] = (dp[d].find(i) != dp[d].end()) ? dp[d][i]+1 : 2;
-                ans = max(ans, dp[d][j]);
-            }
-        }
-        
-        return ans;
-    }
+ public:
+  int longestArithSeqLength(vector<int>& nums) {
+    const int n = nums.size();
+    int ans = 0;
+    // dp[i][k] := length of the longest arithmetic subseq ofnums
+    // nums[0..i] with k = diff + 500
+    vector<vector<int>> dp(n, vector<int>(1001));
+
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < i; ++j) {
+        const int k = nums[i] - nums[j] + 500;
+        dp[i][k] = max(2, dp[j][k] + 1);
+        ans = max(ans, dp[i][k]);
+      }
+
+    return ans;
+  }
 };
