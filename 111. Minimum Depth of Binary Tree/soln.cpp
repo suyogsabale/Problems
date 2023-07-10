e7c624cf2a267ad8ccbd5be7f1e804bf00ce501e
suyogsabale <suyogsabale717@gmail.com>
2023-07-10 21:56:21 +0530

    daily problem

---
+++ 111. Minimum Depth of Binary Tree/soln.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root) return 0;
+        if(root->left && root->right)return 1 + min(minDepth(root->left), minDepth(root->right));
+        //if only one side is not null, we calculate depth using that side
+        return 1 + max(minDepth(root->left), minDepth(root->right));
+    }
+};
